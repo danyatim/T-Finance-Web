096fa9552a6b91f56dcc0c8bacd99508ded47d6b
danyatim <danya_tim@icloud.com>
2025-11-08 12:55:42 +0300

    addservice

--- T-FinanceBackend/Program.cs
+++ T-FinanceBackend/Program.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.RateLimiting;
 using TFinanceBackend.Data;
+using TFinanceBackend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
